bc7a8bf95dca298c48082666d7974646321de579
Al Sutton <al.sutton@alsutton.com>
2022-09-13 12:53:12 +0100

    Add car targets

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,13 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_gta4xlwifi.mk
+    $(LOCAL_DIR)/lineage_gta4xlwifi.mk \
+    $(LOCAL_DIR)/lineage_car_gta4xlwifi.mk
 
 COMMON_LUNCH_CHOICES := \
     lineage_gta4xlwifi-eng \
     lineage_gta4xlwifi-user \
-    lineage_gta4xlwifi-userdebug
+    lineage_gta4xlwifi-userdebug \
+    lineage_car_gta4xlwifi-user \
+    lineage_car_gta4xlwifi-userdebug \
+    lineage_car_gta4xlwifi-eng
---
+++ lineage_car_gta4xlwifi.mk
@@ -0,0 +1,47 @@
+#
+# Copyright (C) 2020-2021 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+## Inherit from generic products, most specific first
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+
+## Inherit from gta4xlwifi device
+$(call inherit-product, device/samsung/gta4xlwifi/device.mk)
+
+## Boot Animation
+TARGET_BOOTANIMATION_HALF_RES := true
+TARGET_SCREEN_HEIGHT := 2000
+TARGET_SCREEN_WIDTH := 1200
+
+## Inherit some common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_full_tablet_wifionly.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_car.mk)
+$(call inherit-product, device/lineage/car/lineage_car_vendor.mk)
+
+# Inherit Snapp modifications
+$(call inherit-product, device/snappautomotive/common/additions.mk)
+
+## Device identifier, this must come after all inclusions
+PRODUCT_DEVICE := gta4xlwifi
+PRODUCT_NAME := lineage_car_gta4xlwifi
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := SM-P610
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_SHIPPING_API_LEVEL := 29
+
+PRODUCT_GMS_CLIENTID_BASE := android-samsung
